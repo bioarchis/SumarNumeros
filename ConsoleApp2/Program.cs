db0456b949ace24079c4db29eef955dd6433af76
JesusDiez <jesdiez@ucm.es>
2026-02-21 23:36:25 +0100

    cambiar programa multiplicar por programa oro

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programa multiplicar");
+            Console.WriteLine("Programa oro");
             Console.WriteLine("Programa SumarNumeros2");
             Console.WriteLine("Programa SumarNumeros2");
             Console.WriteLine("Programa SumarNumeros2");
