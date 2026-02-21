f854999e8fb27fcdc43157392bd95c32b128d434
JesusDiez <jesdiez@ucm.es>
2026-02-21 20:02:38 +0100

    SDAFDSA
    DASDSA

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa SumarNumeros");
+            Console.WriteLine("Programa SumarNumeros2");
+        }
+    }
+}
