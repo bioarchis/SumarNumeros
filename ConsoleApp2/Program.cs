8dc26ff7232cfa7fe470fbad0a34ea6adf5868a3
JesusDiez <jesdiez@ucm.es>
2026-02-22 13:14:17 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,10 +1,10 @@
-namespace ConsoleApp2
+﻿namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programa ojo");
+            Console.WriteLine("Programa caja de rosas");
             Console.WriteLine("Programa libertad");
             Console.WriteLine("Programa SumarNumeros2");
             Console.WriteLine("Programa SumarNumeros2");
